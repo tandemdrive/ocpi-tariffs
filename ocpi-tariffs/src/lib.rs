f4e1b2fbbe94e469c56055beb74fc429033b64e0
Remo <45837188+remkop22@users.noreply.github.com>
2023-03-30 13:11:03 +0200

    Add documentation and README's (#14)
    * add `#[deny(missing_docs)]` and docs
    
    * docs
    
    * readmes
    
    * don't run doctest
    
    * typo
    
    * ignore more doctests
    
    * typo
    
    ---------
    
    Co-authored-by: Remo Pas <remopas@dreamsolution.nl>

--- ocpi-tariffs/src/lib.rs
+++ ocpi-tariffs/src/lib.rs
@@ -1,13 +1,26 @@
+#![deny(missing_docs)]
+#![doc = include_str!("../../README.md")]
+
+/// OCPI specific structures for defining tariffs and charge sessions.
 pub mod ocpi;
+/// Module containing the functionality to price charge sessions with provided tariffs.
 pub mod pricer;
+
 mod restriction;
 mod session;
 mod tariff;
+
+/// OCPI specific numeric types used for calculations, serializing and deserializing.
 pub mod types;
 
-pub type Result<T> = std::result::Result<T, Error>;
+type Result<T> = std::result::Result<T, Error>;
 
+/// Possible errors when pricing a charge session.
 #[derive(Debug)]
 pub enum Error {
+    /// No valid tariff has been found in the list of provided tariffs.
+    ///
+    /// A valid tariff must have a start date time before the start of the session and a end date
+    /// time after the start of the session.
     NoValidTariff,
 }
--- ocpi-tariffs/src/types.rs
+++ ocpi-tariffs/src/types.rs
@@ -1,4 +1,11 @@
+/// OCPI Types related to electricity.
 pub mod electricity;
+
+/// OCPI Types related to monetary amounts.
 pub mod money;
-pub mod number;
+
+/// OCPI Types related to numeric types.
+pub(crate) mod number;
+
+/// OCPI Types related to time and durations.
 pub mod time;
