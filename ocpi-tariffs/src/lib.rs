99d100f961fa0841610a628536eae09ec4a8de8e
Brian Heylin <3947+bheylin@users.noreply.github.com>
2023-04-03 11:10:07 +0200

    Rm thiserror dep (#16)
    It's not worth having an extra dep for just defning a simple error type.
    
    - Move the deps that are only used in the CLI package into it's
      Cargo.toml. Otherwise you're signalling that these deps are common
      when in fact they're only used in one package.
    - Also moved deps specially for the lib into it's Cargo.toml

--- ocpi-tariffs/src/lib.rs
+++ ocpi-tariffs/src/lib.rs
@@ -1,6 +1,8 @@
 #![deny(missing_docs)]
 #![doc = include_str!("../../README.md")]
 
+use std::fmt;
+
 /// OCPI specific structures for defining tariffs and charge sessions.
 pub mod ocpi;
 /// Module containing the functionality to price charge sessions with provided tariffs.
@@ -23,4 +25,12 @@ pub enum Error {
     /// A valid tariff must have a start date time before the start of the session and a end date
     /// time after the start of the session.
     NoValidTariff,
+}
+
+impl std::error::Error for Error {}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("No valid tariff has been found in the list of provided tariffs")
+    }
 }
