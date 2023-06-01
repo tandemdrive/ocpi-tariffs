ef5fb8440f8dc4c55d70aead4660ddb7c74eab55
Remo <remo.pas22@proton.me>
2023-06-01 18:06:24 +0200

    restructure test json's

--- ocpi-tariffs/tests/integration.rs
+++ ocpi-tariffs/tests/integration.rs
@@ -1,30 +1,68 @@
-mod common;
+use chrono_tz::Tz;
+use ocpi_tariffs::{
+    ocpi::{cdr::Cdr, tariff::OcpiTariff},
+    pricer::Pricer,
+};
+use std::path::PathBuf;
 
-#[test]
-fn test_json_files() {
-    let mut should_panic = false;
+#[test_each::file(glob = "ocpi-tariffs/test_data/*/cdr*.json", name(segments = 2))]
+fn test_json(cdr: &str, path: PathBuf) {
+    let tariff = std::fs::read_to_string(path.parent().unwrap().join("tariff.json")).unwrap();
 
-    for json_test in common::collect_json_tests().unwrap() {
-        let tariff = json_test.tariff;
+    let cdr = serde_json::from_str(cdr).unwrap();
+    let tariff = serde_json::from_str(&tariff).unwrap();
 
-        eprintln!("\ntesting directory {:?}", json_test.path);
+    validate_cdr(cdr, tariff).unwrap();
+}
 
-        for (name, cdr) in json_test.cdrs {
-            eprint!("  testing json cdr `{}`: ", name);
+pub fn validate_cdr(cdr: Cdr, tariff: OcpiTariff) -> Result<(), ocpi_tariffs::Error> {
+    let pricer = Pricer::with_tariffs(&cdr, &[tariff], Tz::UTC);
+    let report = pricer.build_report()?;
 
-            let result = std::panic::catch_unwind(|| {
-                common::validate_cdr(cdr, tariff.clone()).unwrap();
-            });
+    assert_eq!(cdr.total_cost, report.total_cost.with_scale(), "total_cost");
 
-            if result.is_err() {
-                should_panic = true;
-            } else {
-                eprintln!("success");
-            }
-        }
-    }
+    assert_eq!(
+        cdr.total_energy,
+        report.total_energy.with_scale(),
+        "total_energy"
+    );
+    assert_eq!(
+        cdr.total_energy_cost.unwrap_or_default(),
+        report.total_energy_cost.with_scale(),
+        "total_energy_cost"
+    );
 
-    if should_panic {
-        panic!("not all json tests succeeded")
-    }
+    assert_eq!(cdr.total_time, report.total_time, "total_time");
+
+    assert_eq!(
+        cdr.total_time_cost.unwrap_or_default(),
+        report.total_time_cost.with_scale(),
+        "total_time_cost"
+    );
+
+    assert_eq!(
+        cdr.total_parking_time.unwrap_or_default(),
+        report.total_parking_time,
+        "total_parking_time"
+    );
+
+    assert_eq!(
+        cdr.total_parking_cost.unwrap_or_default(),
+        report.total_parking_cost.with_scale(),
+        "total_parking_cost"
+    );
+
+    assert_eq!(
+        cdr.total_reservation_cost.unwrap_or_default(),
+        report.total_reservation_cost.with_scale(),
+        "total_reservation_cost"
+    );
+
+    assert_eq!(
+        cdr.total_fixed_cost.unwrap_or_default(),
+        report.total_fixed_cost.with_scale(),
+        "total_fixed_cost"
+    );
+
+    Ok(())
 }
