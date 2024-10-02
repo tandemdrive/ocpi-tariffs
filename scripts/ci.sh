70ac0f6fc8e7d3b24343fd9af2cc4c5b0764d3b2
Brian <brianheylin@dreamsolution.nl>
2024-10-02 11:58:51 +0200

    Bring `script/ci.sh` in line with `.github/workflow/rust.yml`

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,8 +5,9 @@ set -e
 export CARGO_TARGET_DIR="target"
 export RUST_BACKTRACE="full"
 
-cargo deny --workspace -L info check
-cargo check
-cargo clippy
-cargo doc --workspace --no-deps
+cargo deny --workspace --all-features -L info check
+cargo check --workspace --all-features
+cargo fmt --all --check
+cargo clippy --workspace --all-features --all-targets
+cargo doc --workspace --all-features --no-deps --document-private-items
 cargo test
